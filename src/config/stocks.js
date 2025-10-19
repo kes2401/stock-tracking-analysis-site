f2400041fbadb8fd5c5c15ed4466ba0b32cc200e
Keith Scully <keithscully83@gmail.com>
2025-10-19 19:16:27 +0100

    update base stock list

--- src/config/stocks.js
+++ src/config/stocks.js
@@ -9,7 +9,22 @@ export const trackedStocks = [
     { ticker: 'MA', name: 'Mastercard Inc.' },
     { ticker: 'V', name: 'Visa Inc.' },
     { ticker: 'PYPL', name: 'PayPal Holdings Inc.' },
+    { ticker: 'CSCO', name: 'Cisco Sysems Inc.' },
+    { ticker: 'UMG.AS', name: 'Universal Music Group N.V.' },
     { ticker: 'PL', name: 'Planet Labs PBC' },
     { ticker: 'TOST', name: 'Toast Inc.' },
-    { ticker: 'FRSH', name: 'Freshworks Inc' }
+    { ticker: 'FRSH', name: 'Freshworks Inc' },
+    { ticker: 'SPGI', name: 'S&P Global Inc.' },
+    { ticker: 'MCO', name: "Moody's Corporation" },
+    { ticker: '7974.TSE', name: 'Nintendo Co. Ltd.' },
+    { ticker: 'TOI.V', name: 'Topicus.com Inc.' },
+    { ticker: 'BN', name: 'Brookfield Corporation' },
+    { ticker: 'FIG', name: 'Figma Inc.' },
+    { ticker: 'MSCI', name: 'MSCI Inc.' },
+    { ticker: 'ICE', name: 'Intercontinental Exchange Inc.' },
+    { ticker: 'GAW.L', name: 'Games Workshop Group PLC' },
+    { ticker: 'ASML', name: 'ASML Holding N.V.' },
+    { ticker: 'CPRT', name: 'Copart Inc.' },
+    { ticker: 'S', name: 'SentinelOne Inc.' },
+    { ticker: 'WM', name: 'Waste Management Inc.' }
 ];
